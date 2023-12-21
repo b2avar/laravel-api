e51e7b3f645359369b715f5eba059ada6cba8302
Burak Avar <burakavar@Buraks-iMac.local>
2023-12-21 13:50:49 +0300

    InvoiceFilter added.

--- app/Filters/V1/CustomersFilter.php
+++ app/Filters/V1/CustomersFilter.php
@@ -3,7 +3,6 @@
 namespace App\Filters\V1;
 
 use App\Filters\ApiFilter;
-use Illuminate\Http\Request;
 
 class CustomersFilter extends ApiFilter
 {
@@ -28,27 +27,4 @@ class CustomersFilter extends ApiFilter
         'gt' => '>',
         'gte' => '>=',
     ];
-
-    public function transform(Request $request)
-    {
-        $eloQuery = [];
-        foreach ($this->safeParms as $parm => $operators) {
-            $query = $request->query($parm);
-
-            if (! isset($query)) {
-                continue;
-            }
-
-            $column = $this->columnMap[$parm] ?? $parm;
-
-            foreach ($operators as $operator) {
-                if (isset($query[$operator])) {
-                    $eloQuery[] = [$column, $this->operatorMap[$operator], $query[$operator]];
-                }
-            }
-
-        }
-
-        return $eloQuery;
-    }
 }
